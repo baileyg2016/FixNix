344811735c340e03d7e43d945c727e5847f7c25c
Bailey <baileyg2016@gmail.com>
2023-07-29 15:22:36 -0700

    getting all file paths

--- github.py
+++ github.py
@@ -1,14 +1,39 @@
 import os
 import requests
 from dotenv import load_dotenv
+import base64
 
 load_dotenv()
 
 # Make sure to replace 'your-token' with your actual token
-headers = {'Authorization': f'token {os.getenv("GITHUB_TOKEN")}'}
+files_url = 'https://api.github.com/repos/baileyg2016/finTracker/contents'
+headers = {
+    'Authorization': f'Bearer {os.getenv("GITHUB_TOKEN")}',
+    'Accept': 'application/vnd.github.v3+json'
+  }
 
+def decode_file(file):
+    file_content_base64 = file['content']
+    return base64.b64decode(file_content_base64).decode('utf-8')
 
-# Replace 'username' with your username and 'repo' with your repository name
-response = requests.get('https://api.github.com/repos/baileyg2016/', headers=headers)
+def get_all_files(url=files_url):
+    response = requests.get(url, headers=headers)
+    files = response.json()
+    file_paths = []
 
-print(response.json())
+    for file in files:
+        if file['type'] == 'file':
+          file_paths.append(file['path'])
+        elif file['type'] == 'dir':
+          file_paths.extend(get_all_files(file['url']))
+
+    return file_paths
+
+# response = requests.get('https://api.github.com/repos/baileyg2016/finTracker/contents', headers=headers)
+# print(response.json())
+
+# response = requests.get('https://api.github.com/repos/baileyg2016/finTracker/contents/README.md', headers=headers)
+# file_content = decode_file(response.json())
+
+file_paths = get_all_files()
+print(file_paths)
