2f2bf481a8decd00df18487346cc324cde224dd4
Bailey <baileyg2016@gmail.com>
2023-07-29 16:48:27 -0700

    i think my github is good

--- claude.py
+++ claude.py
@@ -5,6 +5,8 @@ import xml.etree.ElementTree as ET
 
 load_dotenv()
 
+anthropic = Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
+
 PROMPT = f"""
   {HUMAN_PROMPT}
   <prompt>
@@ -35,20 +37,66 @@ PROMPT = f"""
 """
 
 
-anthropic = Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
-completion = anthropic.completions.create(
-    model="claude-2",
-    max_tokens_to_sample=10000, # will i need to change this?
-    prompt=PROMPT,
-)
+def create_bug_prompt(code, context):
+  return f"""
+      {HUMAN_PROMPT}
+      <prompt>
+      You are a an AI model tasked with finding bugs in a codebase. 
+      You will be given the details of the bug in <bug></bug> tags.
+      You will be given the codebase in <code></code> tags.
+      The context about what bug you are looking for will be in <context></context> tags.
+      Do your best to find the bug!
+      If you don't know, say you cannot find the bug and then create
+      steps to maybe find the bug in <step></step> tags.
+      Don't output a step tag if you have a solution.
+      If you find the bug, add a <solution></solution> tag with the
+      solution code.
+      Give your explanation in <explanation></explanation> tags.
+      Don't output anything without tags.
+      </prompt>
 
-claude_output = f'<root>{completion.completion}</root>'
+      <context>
+      {context}
+      </context>
+      
+      <code>
+      {code}
+      </code>
 
-root = ET.fromstring(claude_output)
-bug = root.find('bug').text
+
+      Okay go!
+      {AI_PROMPT}
+    """
+
+def create_files_prompt():
+  return f"""
+    {HUMAN_PROMPT}
+    <prompt>
+    You are a an AI model tasked with determining which files,
+    based on their name, are most likely to contain bugs.
+    I will give you a list of files in <files></files> tags.
+    You will output a list of files in <files></files> tags.
+  """
+
+def string_to_xml(string):
+  return ET.fromstring(f'<root>{string}</root>')
+
+
+def find_bug_in_code(code, bug_context):
+  completion = anthropic.completions.create(
+      model="claude-2",
+      max_tokens_to_sample=100000, # will i need to change this?
+      prompt=create_bug_prompt(code, bug_context),
+  )
+  return completion.completion
+
+claude_output = find_bug_in_code("def add_numbers(a, b):\n  result = a + c\n  return result\n\nprint(add_numbers(5, 3))", "The bug is in the add_numbers function")
+print(claude_output)
+root = string_to_xml(claude_output)
+bug = root.find('bug') != None if root.find('bug').text else None
 solution = root.find('solution').text
 explanation = root.find('explanation').text
-steps = root.find('step').text
+steps = root.find('step') != None if root.find('step').text else None
 
 print(f"Bug: {bug}\nSolution: {solution}\nExplanation: {explanation}")
 
--- github.py
+++ github.py
@@ -2,6 +2,7 @@ import os
 import requests
 from dotenv import load_dotenv
 import base64
+import numpy as np
 
 load_dotenv()
 
@@ -16,24 +17,39 @@ def decode_file(file):
     file_content_base64 = file['content']
     return base64.b64decode(file_content_base64).decode('utf-8')
 
-def get_all_files(url=files_url):
+def get_all_files(url):
     response = requests.get(url, headers=headers)
     files = response.json()
-    file_paths = []
+    file_contents = {}
 
     for file in files:
-        if file['type'] == 'file':
-          file_paths.append(file['path'])
+        if file['type'] == 'file' and [file['name'].endswith(ext) for ext in ['.py', '.js', '.ts', '.css']].count(True) > 0:
+            file_response = requests.get(file['url'], headers=headers)
+            file_content = decode_file(file_response.json())
+            file_contents[file['path']] = file_content
         elif file['type'] == 'dir':
-          file_paths.extend(get_all_files(file['url']))
+            file_contents.update(get_all_files(file['url']))
 
-    return file_paths
+    return file_contents
 
-# response = requests.get('https://api.github.com/repos/baileyg2016/finTracker/contents', headers=headers)
-# print(response.json())
+def load_files(url):
+    response = requests.get(url, headers=headers)
+    files = np.array(response.json())
+    file_contents = {}
 
-# response = requests.get('https://api.github.com/repos/baileyg2016/finTracker/contents/README.md', headers=headers)
-# file_content = decode_file(response.json())
+    for file in files:
+        if file['type'] == 'file' and [file['name'].endswith(ext) for ext in ['.py', '.js', '.ts', '.css']].count(True) > 0:
+            file_response = requests.get(file['url'], headers=headers)
+            file_content = decode_file(file_response.json())
+            file_contents[file['path']] = file_content
+        elif file['type'] == 'dir':
+            print(file)
+            print(file['url'])
+            file_contents.update(get_all_files(file['url']))
 
-file_paths = get_all_files()
-print(file_paths)
+    return file_contents
+
+files = get_all_files(files_url)
+print('number of files:', len(files))
+print(files)
+
