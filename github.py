e50ed633afcd78b41bd63b34871f99b6252090cd
Bailey <baileyg2016@gmail.com>
2023-07-29 17:00:32 -0700

    doing some refactoring

--- github.py
+++ github.py
@@ -1,55 +1,33 @@
 import os
 import requests
-from dotenv import load_dotenv
 import base64
-import numpy as np
 
-load_dotenv()
+class GitHubRepo:
+    def __init__(self, repo_url, token=os.getenv('GITHUB_TOKEN')):
+        self.repo_url = repo_url
+        self.headers = {
+            'Authorization': f'Bearer {token}',
+            'Accept': 'application/vnd.github.v3+json'
+        }
 
-# Make sure to replace 'your-token' with your actual token
-files_url = 'https://api.github.com/repos/baileyg2016/finTracker/contents'
-headers = {
-    'Authorization': f'Bearer {os.getenv("GITHUB_TOKEN")}',
-    'Accept': 'application/vnd.github.v3+json'
-  }
+    def decode_file(self, file):
+        file_content_base64 = file['content']
+        return base64.b64decode(file_content_base64).decode('utf-8')
 
-def decode_file(file):
-    file_content_base64 = file['content']
-    return base64.b64decode(file_content_base64).decode('utf-8')
+    def get_all_files(self, url):
+        response = requests.get(url, headers=self.headers)
+        files = response.json()
+        file_contents = {}
 
-def get_all_files(url):
-    response = requests.get(url, headers=headers)
-    files = response.json()
-    file_contents = {}
+        for file in files:
+            if file['type'] == 'file' and [file['name'].endswith(ext) for ext in ['.py', '.js', '.ts', '.css']].count(True) > 0:
+                file_response = requests.get(file['url'], headers=self.headers)
+                file_content = self.decode_file(file_response.json())
+                file_contents[file['path']] = file_content
+            elif file['type'] == 'dir':
+                file_contents.update(self.get_all_files(file['url']))
 
-    for file in files:
-        if file['type'] == 'file' and [file['name'].endswith(ext) for ext in ['.py', '.js', '.ts', '.css']].count(True) > 0:
-            file_response = requests.get(file['url'], headers=headers)
-            file_content = decode_file(file_response.json())
-            file_contents[file['path']] = file_content
-        elif file['type'] == 'dir':
-            file_contents.update(get_all_files(file['url']))
+        return file_contents
 
-    return file_contents
-
-def load_files(url):
-    response = requests.get(url, headers=headers)
-    files = np.array(response.json())
-    file_contents = {}
-
-    for file in files:
-        if file['type'] == 'file' and [file['name'].endswith(ext) for ext in ['.py', '.js', '.ts', '.css']].count(True) > 0:
-            file_response = requests.get(file['url'], headers=headers)
-            file_content = decode_file(file_response.json())
-            file_contents[file['path']] = file_content
-        elif file['type'] == 'dir':
-            print(file)
-            print(file['url'])
-            file_contents.update(get_all_files(file['url']))
-
-    return file_contents
-
-files = get_all_files(files_url)
-print('number of files:', len(files))
-print(files)
-
+    def load_files(self):
+        return self.get_all_files(self.repo_url)
